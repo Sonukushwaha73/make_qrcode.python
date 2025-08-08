f59d80eef4d750820b8bfbae29fe433443855f9b
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-08 08:29:56 +0530

    Add files via upload

---
+++ make.qr2.py
@@ -0,0 +1,18 @@
+
+import qrcode
+
+
+
+from PIL import Image
+
+
+qr=qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H,
+                 box_size=10,border=4,)
+
+
+qr.add_data("https://github.com/Sonukushwaha73?tab=repositories")
+qr.make(fit =True)
+img=qr.make_image(fill_color="black" ,back_color="red")
+img.save("github.png")
+
